48556b949f60c0700459f7022512611739ccd4ae
Osama Odeh <121430755+OsamahKhOdeh@users.noreply.github.com>
2023-02-11 15:48:18 +0400

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { createProduct } from "../controllers/products.js";
+import { createProduct, getProducts } from "../controllers/products.js";
 
 const router = express.Router();
 //import auth from "../middleware/auth.js";
 
 //router.get("/search", getPostsBySearch);
-//router.get("/", getPosts);
+router.get("/", getProducts);
 //0router.get("/:id", getPost);
 
 router.post("/", createProduct);
