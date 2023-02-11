24f512efa4816f166779ed52239e7dec70cd590f
Osama Odeh <121430755+OsamahKhOdeh@users.noreply.github.com>
2023-02-11 15:14:28 +0400

    setHeader 2

--- controllers/products.js
+++ controllers/products.js
@@ -12,7 +12,11 @@ export const createProduct = async (req, res) => {
 
   try {
     await newProduct.save();
-res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.status(201).json(newProduct);
   } catch (error) {
     res.status(409).json({ message: error.message });
